19624a6b6e8d50e6fb0fad1a0ba55ae9c8fcb375
THOMAS RAMESH <113359232+thomas2570@users.noreply.github.com>
2025-11-04 14:19:00 +0000

    Container config done

---
+++ basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as version
+
+// alert(3+3) we are using nodejs, not using browser
+
+let name = "Thomas"
+let age = 21
+let isLoggedIn = false
+
+// number = 2 to poer 53
+//bigint
+//string = ""
+// boolean = true/false
+//null= standlone value
+//undefined = 
+//symbol = used in react (unique)
+//object
+
+
+console.log(typeof null); // interviwe question what is null = object(ans)
+console.log(typeof undefined); // fpr value
+
+console.log("Thomas Ramesh");
