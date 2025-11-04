62ca27cbb1a8f8b4ab2de259ceb5ddc22d91ebec
THOMAS RAMESH <113359232+thomas2570@users.noreply.github.com>
2025-11-04 18:40:27 +0000

    Change

---
+++ basics/conversionOperation.js
@@ -0,0 +1,40 @@
+// let score = "33abc" =(string)= NaN
+// let score = null= (0)
+  // let score = true = 1, false = 0
+let score = "Thomas"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+// 1 = true, 0 = false
+// "" = false
+//"Thomas"=true
+
+
+// ************************Operation*********************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "Thomas"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
