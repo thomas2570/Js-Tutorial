c63736b2f3afa0d42c0b91d3fdce7768c20263ba
THOMAS RAMESH <113359232+thomas2570@users.noreply.github.com>
2025-11-05 08:49:14 +0000

    datatype changes

---
+++ basics/datatypesummary.js
@@ -0,0 +1,48 @@
+// Primitive data type
+// 7 types 
+// 1 String
+// 2 Number
+// 3 Boolean
+// 4 Null
+// 5 Undefined
+// 6 Symbol
+// 7 BigInt
+
+const score = 100;
+const scoreValue = 100.5;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+
+const id = Symbol("1234");
+const anotherId = Symbol("1234");
+
+console.log(id === anotherId);
+
+const bigNumber = 3456789012345678901234567890n;
+
+const heros = ["Shaktiman", "Naagraj", "Doga"];
+let myObj = 
+{
+    name:"Thomas",
+    age: 30,
+}
+
+// Non-primitive data type
+// 1 Object
+
+// Example of each primitive data type
+
+// referenceTypes (Non primitive data type)
+// Array 
+// Object
+// Functions
+
+function myfunction = function(){
+console.log("Hello World");
+}
+
+
+console.log(typeof bigNumber);
