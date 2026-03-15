fe547c93e9b161622ac61aca4c05606a84682acc
thomas2570 <thomasramesh449@gmail.com>
2026-03-16 01:57:44 +0530

    change

---
+++ Array/array.js
@@ -0,0 +1,42 @@
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["ironman","Captain","Thor"] 
+
+
+const myArr2 = (1,2,3,4)
+// console.log(myArr[1]);
+
+// Array Method
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop(2)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9)); // false
+// console.log(myArr.indexOf(9)); // -1
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
