4c056927146dbb4698fc883e1b14ba750ae62c58
thomas2570 <thomasramesh449@gmail.com>
2026-03-16 01:12:22 +0530

    Initial commit

---
+++ Basic/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using nodejs, not using browser
+
+let name = "Thomas"
+let age = 21
+let isLoggedIn = false
+
+// number = range 2 to power 53
+//bigint = facebook , Trading,stock market
+//string = ""
+// boolean = true/false
+//null= standlone value
+//undefined = 
+//symbol = used in react (unique)
+//object
+
+
+console.log(typeof null); // interviwe question what is null = object(ans)
+console.log(typeof undefined); // fpr value
+
+console.log("Thomas Ramesh");
---
+++ Math/nums.js
@@ -0,0 +1,47 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length); // 3
+
+// console.log(balance.toFixed(3)); // 100.000 print last digit(000) after point
+
+const otherNumber = 23.9966
+// console.log(otherNumber.toPrecision(3)); // 24.0
+
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN')); // 1,00,000
+
+
+
+//----------------- Maths----------------------------------
+
+console.log(Math);
+
+// console.log(Math.abs(-4)); // Print 4;
+
+// console.log(Math.round(4.6)); // 5
+
+// console.log(Math.ceil(4.2)); //  5
+// console.log(Math.floor(4.9)); // 4
+
+//console.log(Math.min(4,3,6,7)); // array lowest value  3
+// console.log(Math.max(4,3,6,7)); // Upper value 7
+
+console.log(Math);
+console.log(Math.random()); // 0.5085464816454968
+
+console.log((Math.random()*10) + 1); // 6.012991985810249
+
+console.log(Math.floor(Math.random()*10) + 1); // 4
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// 2,3,1,5 Print 1 to 6 any value
+
