894025ca5efbb8487c0c865e5884705d2b3785f4
thomas2570 <thomasramesh449@gmail.com>
2026-03-17 01:30:29 +0530

    change

---
+++ Objects/object2.js
@@ -0,0 +1,54 @@
+// const  tinderUser = new Object() // Singleton object
+const tinderUser = {} // non singleton object
+
+tinderUser.Id = "123abc"
+tinderUser.name = "Ankit"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "Thomas@gmail.com",
+    fullname:{
+        userFullname:{
+        firstname: "Thomas",
+        lastname: "Ramesh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullnam.lastname);
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+    id: 1,
+    email: "thomas@gmail.com"
+    },
+    {
+    id: 1,
+    email: "thomas@gmail.com"
+    },
+    {
+    id: 1,
+    email: "thomas@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
