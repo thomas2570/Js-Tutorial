e94fbd3e53abef921020018b347a86bb1cb9d231
thomas2570 <thomasramesh449@gmail.com>
2026-04-09 10:48:31 +0530

    change

--- Function/function.js
+++ Function/function.js
@@ -44,4 +44,33 @@ function loginUserMessage(username= "Thomas"){
 // console.log(loginUserMessage("Thomas"))
 console.log(loginUserMessage()) // empty value maan liya jata hai ki wo false value hai
 
+function  calculateCartPrice(...num1){
+// function  calculateCartPrice(val1,val2, ...num1){ // return only 500
+   return num1
+}
 
+console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username: "Thomas",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
