2cf9c22873bf7bae49777ed30d71818380f4a2f4
thomas2570 <thomasramesh449@gmail.com>
2026-04-09 10:15:24 +0530

    JSON

---
+++ Objects/object3.js
@@ -0,0 +1,34 @@
+const course = {
+    coursename: "Js in hindi",
+    price: "9999",
+    curseInstructor: "Thomas"
+}
+// course.curseInstructor
+
+const {curseInstructor: instructor} = course
+
+console.log(instructor);
+
+//----Destructure of object------
+
+// const navbar= ({company}) => {
+
+// }
+// navbar(company="Thomas")/
+
+//------ JSON IN OBJECT FORM ------
+
+// {
+//     "name": "Thomas",
+//     "price": "free"
+// }
+
+//-------- JSON IN ARRAY FORM
+
+[
+    {},
+    {},
+    {}
+]
+
+//-----  API MAY BE ARRAY OR OBJECT BOTH ------
