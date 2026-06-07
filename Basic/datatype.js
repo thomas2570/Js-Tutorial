47628ec8803e69970d41535af2b0acc60eb98779
thomas2570 <thomasramesh449@gmail.com>
2026-06-08 00:11:46 +0530

    commit

--- Basic/datatype.js
+++ Basic/datatype.js
@@ -7,16 +7,38 @@ let age = 21
 let isLoggedIn = false
 
 // number = range 2 to power 53
-//bigint = facebook , Trading,stock market
-//string = ""
+
+//bigint = facebook , Trading,stock market(last me n lagate hai)
+
+//string = "",'', ``,
 // boolean = true/false
 //null= standlone value
 //undefined = 
-//symbol = used in react (unique)
+//symbol = used in react (unique) 
+// future main hum libraries use karenge ab is case main us un libraries 
+
+
 //object
 
 
 console.log(typeof null); // interviwe question what is null = object(ans)
-console.log(typeof undefined); // fpr value
+console.log(typeof undefined); // fpr value,  apne ak variable banaya use value nhi di to jo value by default mili wo hai undefined
 
 console.log("Thomas Ramesh");
+
+
+// primitive - > aisi sari values jinko copye karne per tumhe ak real copy mil jayee
+//string, num,boolean, null, undefined,symbol,bigint
+
+// reference -> inko copy karne per real copy nhi milegi but apko reference milega parent ka
+// array, objects, function
+// [],{},()
+
+
+
+// -------------------   Dynamic typing    -------------------------------------------
+
+// js me static typing nhi hai and yha per dynamic typing hai jiska mtlb hai ap data change kar sakte hai  
+
+// type coercion -> concept jsimein apka ak type automatically convert ho jayega
+
