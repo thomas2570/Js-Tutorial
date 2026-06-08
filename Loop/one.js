d3aea797f4ef4f8374580630e82a51d44a1890fe
thomas2570 <thomasramesh449@gmail.com>
2026-06-08 23:28:24 +0530

    change

---
+++ Loop/one.js
@@ -0,0 +1,34 @@
+// repeat karne ko loop kahte hai
+
+// kaha se jana hai kaha tak jana hai kaise jana hai 
+// for loop
+
+// kaha se jana hai kab rukna hai kaise jana hai
+// while loop
+
+// ----------- for ----------------------------------------------
+
+// 1-100
+
+for(let i = 1; i<101; i++){
+      console.log(i);
+      
+}
+
+// -------------------- while -------------------------------
+
+let j = 1;
+while(j<32){
+    console.log(j);
+    i++;
+}
+
+// ----------------do while ---------------------------
+let k = 12;
+do{
+    console.log(k);
+    
+    i++;
+}
+while(i<20)
+    // AK BAAR JARUR CHALEGA 
