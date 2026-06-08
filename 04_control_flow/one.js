566d23efdda21f9af9c7cca59491f89abb316b1f
thomas2570 <thomasramesh449@gmail.com>
2026-06-08 05:41:18 +0530

    commit

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,6 +2,8 @@
 //const isUserLoggedIn = true
 // const temperature = 41
 
+const { use } = require("react")
+
 // if(temperature>50) {
 //    console.log("less than 50");
 // } else {
@@ -45,4 +47,29 @@ if(isUserLoggedIn && debitCard && 2==3){
 }
 if(loggedInFromEmail || loggedInFromGoogle){
     console.log("User logged in");
+}
+
+
+function getGrade(score){
+    if(score >= 90 && score <= 100){
+        return "A";
+    } else if(score >= 80 && score < 90){
+        return "B";
+    }
+    else if(score >= 70 && score <= 89){
+        return "C";
+    }
+    else{
+        return "Invalid";
+    }
+}
+console.log(getGrade(78));
+
+// Rock paper secissors logic 
+
+function rps(user,computer){
+
+    if(user === "rock" && computer === rock)
+        return "user";
+    rps("rock","scissor");
 }
