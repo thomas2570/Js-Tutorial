87fc6aa25e6b1835f2d12db1523e649a226bf0d0
thomas2570 <thomasramesh449@gmail.com>
2026-06-12 23:47:40 +0530

    commit

---
+++ Project/Rendom Color/script.js
@@ -0,0 +1,31 @@
+//generate a random color
+
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const changeBgColor = function () {
+  document.body.style.background = randomColor();
+};
+
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+};
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
